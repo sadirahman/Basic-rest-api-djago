669bf7f014612869d169e60b004366265d9682cf
Sadi rahman <42184120+sadirahman@users.noreply.github.com>
2019-01-30 10:11:38 +0600

    rest_api

---
+++ restapp/products/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class product(models.Model):
+    created = models.DateTimeField(auto_now_add =True)
+    name = models.CharField(max_length=30)
+    description = models.TextField(blank =True, default ='')
+    price = models.DecimalField(max_digits=10, decimal_places =2)
+
+    def __str__(self):
+        return self.name
+
+
---
+++ restapp/products/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import product
+from .serializers import ProductSerializer
+from rest_framework import status
+
+
+
+# Create your views here.
+@api_view(['GET','POST'])
+def product_list(request):
+    if request.method == 'GET':
+        obj = product.objects.all()
+        serializer = ProductSerializer(obj, many=True)
+        return Response(serializer.data)
+    elif request.method =='POST':
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','PUT'])
+def product_detail(request, pk):
+    try:
+        obj = product.objects.get(id =pk)
+    except product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method =='GET':
+        serializer =ProductSerializer(obj)
+        return Response(serializer.data)
+    elif request.method =='PUT':
+        serializer =ProductSerializer(obj,data =request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
